578d0217bc7c81074708fede6bdfb98c645db5af
David Budworth <dbudworth@peak6.com>
2014-12-30 14:29:29 -0600

    cleaned up api

--- mmd/mmd.go
+++ mmd/mmd.go
@@ -3,6 +3,7 @@ package mmd
 import (
 	"encoding/binary"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	logpkg "log"
@@ -14,22 +15,77 @@ import (
 )
 
 var log = logpkg.New(os.Stdout, "[mmd] ", logpkg.LstdFlags|logpkg.Lmicroseconds)
+var mmdUrl = "localhost:9999"
+
+func init() {
+	flag.StringVar(&mmdUrl, "mmd", mmdUrl, "Sets default MMD Url")
+}
 
 // EOC Signals close of MMD channel
 var EOC = errors.New("End Of Channel")
 
 // ServiceFunc Handler callback for registered services
-type ServiceFunc func(*MMDConn, *MMDChan, *ChannelCreate)
+type ServiceFunc func(*Conn, *Chan, *ChannelCreate)
 
-type config struct {
-	url     string
-	readSz  int
-	writeSz int
-	appName string
+type Config struct {
+	Url     string
+	ReadSz  int
+	WriteSz int
+	AppName string
 }
 
-// MMDConn Connection and channel dispatch map
-type MMDConn struct {
+func NewConfig(url string) *Config {
+	return &Config{
+		Url:     url,
+		ReadSz:  64 * 1024,
+		WriteSz: 64 * 1024,
+		AppName: fmt.Sprintf("Go:%s", os.Args[0]),
+	}
+}
+
+func (c *Config) Connect() (*Conn, error) {
+	return _create_connection(c)
+}
+
+// Creates a default URL connection (-mmd to override)
+func Connect() (*Conn, error) {
+	return ConnectTo(mmdUrl)
+}
+func LocalConnect() (*Conn, error) {
+	return ConnectTo("localhost:9999")
+}
+func ConnectTo(url string) (*Conn, error) {
+	return NewConfig(url).Connect()
+}
+
+func _create_connection(cfg *Config) (*Conn, error) {
+	addr, err := net.ResolveTCPAddr("tcp", cfg.Url)
+	if err != nil {
+		return nil, err
+	}
+	// log.Printf("Connecting to: %s / %s\n", cfg.url, addr)
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return nil, err
+	}
+	conn.SetWriteBuffer(cfg.WriteSz)
+	conn.SetReadBuffer(cfg.ReadSz)
+	mmdc := &Conn{socket: conn,
+		writeChan:   make(chan []byte),
+		dispatch:    make(map[ChannelId]chan ChannelMsg, 1024),
+		callTimeout: time.Second * 5,
+		services:    make(map[string]ServiceFunc),
+	}
+	go writer(mmdc)
+	go reader(mmdc)
+	handshake := []byte{1, 1}
+	handshake = append(handshake, cfg.AppName...)
+	mmdc.WriteFrame(handshake)
+	return mmdc, nil
+}
+
+// Conn Connection and channel dispatch map
+type Conn struct {
 	socket      *net.TCPConn
 	writeChan   chan []byte
 	dispatch    map[ChannelId]chan ChannelMsg
@@ -38,21 +94,21 @@ type MMDConn struct {
 	services    map[string]ServiceFunc
 }
 
-// MMDChan MMD Channel
-type MMDChan struct {
+// Chan MMD Channel
+type Chan struct {
 	ch  chan ChannelMsg
-	con *MMDConn
+	con *Conn
 	Id  ChannelId
 }
 
-func (c *MMDChan) NextMessage() (ChannelMsg, error) {
+func (c *Chan) NextMessage() (ChannelMsg, error) {
 	a, ok := <-c.ch
 	if !ok {
 		return ChannelMsg{}, EOC
 	}
 	return a, nil
 }
-func (c *MMDChan) Close(body interface{}) error {
+func (c *Chan) Close(body interface{}) error {
 	cm := ChannelMsg{Channel: c.Id, Body: body, IsClose: true}
 	c.con.unregisterChannel(c.Id)
 	buff := NewBuffer(1024)
@@ -63,7 +119,7 @@ func (c *MMDChan) Close(body interface{}) error {
 	c.con.Send(buff.Flip())
 	return nil
 }
-func (c *MMDChan) Send(body interface{}) error {
+func (c *Chan) Send(body interface{}) error {
 	cm := ChannelMsg{Channel: c.Id, Body: body}
 	buff := NewBuffer(1024)
 	err := Encode(buff, cm)
@@ -84,14 +140,14 @@ func Call(service string, body interface{}) (interface{}, error) {
 	return lc.Call(service, body)
 }
 
-func (c *MMDConn) SetDefaultCallTimeout(dur time.Duration) {
+func (c *Conn) SetDefaultCallTimeout(dur time.Duration) {
 	c.callTimeout = dur
 }
-func (c *MMDConn) GetDefaultCallTimeout() time.Duration {
+func (c *Conn) GetDefaultCallTimeout() time.Duration {
 	return c.callTimeout
 }
 
-func (c *MMDConn) registerServiceUtil(name string, fn ServiceFunc, registryAction string) error {
+func (c *Conn) registerServiceUtil(name string, fn ServiceFunc, registryAction string) error {
 	c.services[name] = fn
 	ok, err := c.Call("serviceregistry", map[string]interface{}{
 		"action": registryAction,
@@ -106,56 +162,15 @@ func (c *MMDConn) registerServiceUtil(name string, fn ServiceFunc, registryActio
 	return err
 }
 
-func (c *MMDConn) RegisterLocalService(name string, fn ServiceFunc) error {
+func (c *Conn) RegisterLocalService(name string, fn ServiceFunc) error {
 	return c.registerServiceUtil(name, fn, "registerLocal")
 }
 
-func (c *MMDConn) RegisterService(name string, fn ServiceFunc) error {
+func (c *Conn) RegisterService(name string, fn ServiceFunc) error {
 	return c.registerServiceUtil(name, fn, "register")
 }
 
-func newConfig(url string) *config {
-	return &config{
-		url:     url,
-		readSz:  64 * 1024,
-		writeSz: 64 * 1024,
-		appName: fmt.Sprintf("Go:%s", os.Args[0]),
-	}
-}
-
-func LocalConnect() (*MMDConn, error) {
-	return Connect(newConfig("localhost:9999"))
-}
-func ConnectTo(host string, port int) (*MMDConn, error) {
-	return Connect(newConfig(fmt.Sprintf("%s:%d", host, port)))
-}
-func Connect(cfg *config) (*MMDConn, error) {
-	addr, err := net.ResolveTCPAddr("tcp", cfg.url)
-	if err != nil {
-		return nil, err
-	}
-	// log.Printf("Connecting to: %s / %s\n", cfg.url, addr)
-	conn, err := net.DialTCP("tcp", nil, addr)
-	if err != nil {
-		return nil, err
-	}
-	conn.SetWriteBuffer(cfg.writeSz)
-	conn.SetReadBuffer(cfg.readSz)
-	mmdc := &MMDConn{socket: conn,
-		writeChan:   make(chan []byte),
-		dispatch:    make(map[ChannelId]chan ChannelMsg, 1024),
-		callTimeout: time.Second * 5,
-		services:    make(map[string]ServiceFunc),
-	}
-	go writer(mmdc)
-	go reader(mmdc)
-	handshake := []byte{1, 1}
-	handshake = append(handshake, cfg.appName...)
-	mmdc.WriteFrame(handshake)
-	return mmdc, nil
-}
-
-func (c *MMDConn) Subscribe(service string, body interface{}) (*MMDChan, error) {
+func (c *Conn) Subscribe(service string, body interface{}) (*Chan, error) {
 	buff := NewBuffer(1024)
 	cc := NewChannelCreate(SubChan, service, body)
 	err := Encode(buff, cc)
@@ -165,14 +180,14 @@ func (c *MMDConn) Subscribe(service string, body interface{}) (*MMDChan, error) 
 	ch := make(chan ChannelMsg, 1)
 	c.registerChannel(cc.ChannelId, ch)
 	c.Send(buff.Flip())
-	return &MMDChan{ch: ch, con: c, Id: cc.ChannelId}, nil
+	return &Chan{ch: ch, con: c, Id: cc.ChannelId}, nil
 }
 
-func (c *MMDConn) Call(service string, body interface{}) (interface{}, error) {
+func (c *Conn) Call(service string, body interface{}) (interface{}, error) {
 	return c.CallAuthenticated(service, AuthToken(NO_AUTH_TOKEN), body)
 }
 
-func (c *MMDConn) CallAuthenticated(service string, token AuthToken, body interface{}) (interface{}, error) {
+func (c *Conn) CallAuthenticated(service string, token AuthToken, body interface{}) (interface{}, error) {
 	buff := NewBuffer(1024)
 	cc := NewChannelCreate(CallChan, service, body)
 	cc.AuthToken = token
@@ -196,12 +211,12 @@ func (c *MMDConn) CallAuthenticated(service string, token AuthToken, body interf
 	}
 }
 
-func (c *MMDConn) registerChannel(cid ChannelId, ch chan ChannelMsg) {
+func (c *Conn) registerChannel(cid ChannelId, ch chan ChannelMsg) {
 	c.dlock.Lock()
 	c.dispatch[cid] = ch
 	c.dlock.Unlock()
 }
-func (c *MMDConn) unregisterChannel(cid ChannelId) chan ChannelMsg {
+func (c *Conn) unregisterChannel(cid ChannelId) chan ChannelMsg {
 	c.dlock.Lock()
 	ret, ok := c.dispatch[cid]
 	if ok {
@@ -210,25 +225,25 @@ func (c *MMDConn) unregisterChannel(cid ChannelId) chan ChannelMsg {
 	c.dlock.Unlock()
 	return ret
 }
-func (c *MMDConn) lookupChannel(cid ChannelId) chan ChannelMsg {
+func (c *Conn) lookupChannel(cid ChannelId) chan ChannelMsg {
 	c.dlock.RLock()
 	ret := c.dispatch[cid]
 	c.dlock.RUnlock()
 	return ret
 }
 
-func (c MMDConn) String() string {
-	return fmt.Sprintf("MMDConn{remote: %s, local: %s}", c.socket.RemoteAddr(), c.socket.LocalAddr())
+func (c Conn) String() string {
+	return fmt.Sprintf("Conn{remote: %s, local: %s}", c.socket.RemoteAddr(), c.socket.LocalAddr())
 }
 
-func (c *MMDConn) Send(buff *Buffer) {
+func (c *Conn) Send(buff *Buffer) {
 	c.writeChan <- buff.Bytes()
 }
-func (c *MMDConn) Close() {
+func (c *Conn) Close() {
 	close(c.writeChan)
 }
 
-func cleanupReader(c *MMDConn) {
+func cleanupReader(c *Conn) {
 	log.Println("Cleaning up reader")
 	c.socket.CloseRead()
 	c.dlock.Lock()
@@ -238,7 +253,7 @@ func cleanupReader(c *MMDConn) {
 	}
 }
 
-func reader(c *MMDConn) {
+func reader(c *Conn) {
 	fszb := make([]byte, 4)
 	buff := make([]byte, 256)
 	defer cleanupReader(c)
@@ -300,7 +315,7 @@ func reader(c *MMDConn) {
 				}
 				ch := make(chan ChannelMsg, 1)
 				c.registerChannel(msg.ChannelId, ch)
-				fn(c, &MMDChan{ch: ch, con: c, Id: msg.ChannelId}, &msg)
+				fn(c, &Chan{ch: ch, con: c, Id: msg.ChannelId}, &msg)
 			default:
 				log.Panic("Unknown message type:", reflect.TypeOf(msg), msg)
 			}
@@ -308,7 +323,7 @@ func reader(c *MMDConn) {
 	}
 }
 
-func writer(c *MMDConn) {
+func writer(c *Conn) {
 	fsz := make([]byte, 4)
 	for {
 		select {
@@ -332,6 +347,6 @@ func writer(c *MMDConn) {
 	}
 }
 
-func (c *MMDConn) WriteFrame(data []byte) {
+func (c *Conn) WriteFrame(data []byte) {
 	c.writeChan <- data
 }
